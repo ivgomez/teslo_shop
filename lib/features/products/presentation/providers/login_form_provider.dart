f830a7cb4c083842bc5f851650b5acae8554c87a
ivgomez <iv.gomez@hotmail.com>
2025-03-05 20:09:00 -0500

    creating and connecting login notifier and provider

--- lib/features/products/presentation/providers/login_form_provider.dart
+++ lib/features/products/presentation/providers/login_form_provider.dart
@@ -1,23 +1,90 @@
-// 1 Provider state
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:formz/formz.dart';
 import 'package:teslo_shop/features/shared/shared.dart';
 
-class LoginFormProvider {
+// 3 StateNotifierProvider - this is consumed outside
+final loginFormProvider = StateNotifierProvider.autoDispose<LoginFormNotifier, LoginFormState>((ref) {
+  return LoginFormNotifier();
+});
+
+
+// 1 Provider state
+class LoginFormState {
   final bool isPosting;
   final bool isFormPosted;
   final bool isValid;
   final Email email;
   final Password password;
 
-  LoginFormProvider({
+  LoginFormState({
     this.isPosting = false,
     this.isFormPosted = false,
     this.isValid = false,
     this.email = const Email.pure() ,
     this.password = const Password.pure(),
   });
+
+  LoginFormState copyWitch({
+    bool? isPosting,
+    bool? isFormPosted,
+    bool? isValid,
+    Email? email,
+    Password? password,
+  }) => LoginFormState(
+    isPosting: isPosting ?? this.isPosting,
+    isFormPosted: isFormPosted ?? this.isFormPosted,
+    isValid: isValid ?? this.isValid,
+    email: email ?? this.email,
+    password: password ?? this.password,
+  );
+
+  @override
+  String toString() {
+    return '''
+LoginFormState:
+  isPosting: $isPosting
+  isFormPosted: $isFormPosted
+  isValid: $isValid
+  email: $email
+  password: $password
+''';
+  }
 }
 
 // 2 How to implement the Notifier
+class LoginFormNotifier extends StateNotifier<LoginFormState> {
+  LoginFormNotifier(): super( LoginFormState() );
 
-// 3 StateNotifierProvider - this is consumed outside
+  onEmailChange(String value) {
+    final newEmail = Email.dirty(value);
+    state = state.copyWitch(
+      email: newEmail,
+      isValid: Formz.validate([newEmail, state.password]),
+    );
+  }
+
+  onPasswordChange(String value) {
+    final newPassword = Password.dirty(value);
+    state = state.copyWitch(
+      password: newPassword,
+      isValid: Formz.validate([state.email, newPassword]),
+    );
+  }
+
+  onFormSubmit() {
+    _touchEveryField();
+    if (! state.isValid ) return;
+    print(state);   
+  }
+
+  _touchEveryField() {
+    final email = Email.dirty(state.email.value);
+    final password = Password.dirty(state.password.value);
+    state = state.copyWitch(
+      isFormPosted: true,
+      email: email,
+      password: password,
+      isValid: Formz.validate([email, password]),
+    );
+  }
+}
