0104e643370eafe8959e20da6a509fe6309d1455
ivgomez <iv.gomez@hotmail.com>
2025-03-09 20:01:25 -0500

    product details: testing the API to update a product

--- lib/features/products/presentation/providers/forms/product_form_provider.dart
+++ lib/features/products/presentation/providers/forms/product_form_provider.dart
@@ -42,7 +42,7 @@ class ProductFormNotifier extends StateNotifier<ProductFormState> {
     _touchedEverything();
 
     if( !state.isFormValid ) return false;
-    if ( onSubmitCallback == null ) return false;
+    //if ( onSubmitCallback == null ) return false;
 
     final productLike = {
       'id' : state.id,
