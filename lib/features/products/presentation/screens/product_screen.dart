d68a4dc73802f1b1485e8f2931e18d972413f1b9
ivgomez <iv.gomez@hotmail.com>
2025-03-10 20:01:45 -0500

    product details: hide keyboard after update/create

--- lib/features/products/presentation/screens/product_screen.dart
+++ lib/features/products/presentation/screens/product_screen.dart
@@ -27,33 +27,36 @@ class ProductScreen extends ConsumerWidget {
 
     final productState = ref.watch(productProvider(productId));
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Edit Product'),
-        actions: [
-          IconButton(
-            onPressed: (){}, 
-            icon: const Icon( Icons.camera_alt_outlined )
-          )
-        ],
-      ),
-      body: productState.isLoading
-        ? const FullScreenLoader()
-        : _ProductView(product: productState.product!),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          if ( productState.product == null ) return;
-
-          ref.read(
-            productFormProvider(productState.product!).notifier
-          ).onFormSubmit()
-          .then((value) {
-            if ( !value ) return;
-            if (context.mounted) showSnackBar(context);
-          });
-        },
-        
-        child: const Icon( Icons.save_as_outlined ),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Edit Product'),
+          actions: [
+            IconButton(
+              onPressed: (){}, 
+              icon: const Icon( Icons.camera_alt_outlined )
+            )
+          ],
+        ),
+        body: productState.isLoading
+          ? const FullScreenLoader()
+          : _ProductView(product: productState.product!),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            if ( productState.product == null ) return;
+      
+            ref.read(
+              productFormProvider(productState.product!).notifier
+            ).onFormSubmit()
+            .then((value) {
+              if ( !value ) return;
+              if (context.mounted) showSnackBar(context);
+            });
+          },
+          
+          child: const Icon( Icons.save_as_outlined ),
+        ),
       ),
     );
   }
@@ -215,6 +218,7 @@ class _SizeSelector extends StatelessWidget {
       }).toList(), 
       selected: Set.from( selectedSizes ),
       onSelectionChanged: (newSelection) {
+        FocusScope.of(context).unfocus();
         onSizesChanged( List.from( newSelection ) );
       },
       multiSelectionEnabled: true,
@@ -257,6 +261,7 @@ class _GenderSelector extends StatelessWidget {
         }).toList(), 
         selected: { selectedGender },
         onSelectionChanged: (newSelection) {
+          FocusScope.of(context).unfocus();
           onGenderChanged(newSelection.first);
         },
       ),
